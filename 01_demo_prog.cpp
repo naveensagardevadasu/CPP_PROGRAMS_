d50d4ff08d8a0f42d5cb70674c902cd9ba37022f
naveensagardevadasu <naveensagard620@gmail.com>
2022-10-23 22:44:55 -0700

    first test code for printing in possible ways
    basic demo code in CPP which includes printing in different approach

---
+++ 01_demo_prog.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int variable_int=10;
+    cout<<"hello world"<<endl; //print a string on the output screen
+    cout<<variable_int<<endl; // pritn a integer type variable 
+    cout<<1.344<<endl; //print a direct float value
+    cout<<'*'<<endl; //print a char value
+    return 0;
+}
